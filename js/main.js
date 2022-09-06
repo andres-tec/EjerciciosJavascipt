0f174f7870b33d680ff2a46bebb27c61395feaf1
andres <andrestecpile97@gmail.com>
2022-09-06 13:20:33 -0500

    se creo el ejercicio 4

--- js/main.js
+++ js/main.js
@@ -1,24 +1,21 @@
-let array_1 = [1, 2, 0, 8, 6, 7, 9];
+let array_4 = [1, 2, 5, 14, 24, 31, 50, 105];
+let array_5 = [];
+let paso1 = 0;
+let paso2 = 0;
+let index_2 = 1;
+let index = 0;
+for (index = 0; index < array_4.length; index++) {
 
-let menor = 0;
-let mayor = 0;
-let base = 0;
-let index_1 = 1;
-for (let index = 0; index < array_1.length; index++) {
-
-
-    if (array_1[index_1] >= array_1[index]) {
-        mayor = array_1[index_1];               //2 2
-        console.log(array_1[index_1]);
-
-
-    } else {
-        mayor = array_1[index]; //2 8
-        console.log(array_1[index]);
-        index_1++;
-    }
-
-
+    paso1 = array_4[index];  //1  
+    paso2 = array_4[index_2];    //2  
+    array_4[index] = paso2;  // 2 
+    array_4[index_2] = paso1;//1  
+    index_2++;  //2
+    index++;
 }
 
-console.log("el numero mayor en el vector es:", mayor);
+
+for (let index = 0; index < array_4.length; index++) {
+    console.log("lista desordenada:", array_4[index]);
+
+}
